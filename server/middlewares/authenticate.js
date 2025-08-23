7fbe85a0732e6ad0d7cebd5ba64e3ff0530c2844
venkyreddy <venkyreddyy2004@gmail.com>
2025-08-24 01:02:45 +0530

    updated profile

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
-import userModel from "../models/userModel.js";
+
 
 export const authenticate = async (req, res, next) => {
   try {
