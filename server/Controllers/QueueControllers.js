dc1a4ebf1a0a5265dd9f314b60f79f0a041d2374
udaykiran1150 <udaykiranponnaganti6@gmail.com>
2025-08-24 01:13:07 +0530

    route added

--- server/Controllers/QueueControllers.js
+++ server/Controllers/QueueControllers.js
@@ -23,30 +23,49 @@ catch(err){
 
 }
 
-export const TreatedByDoctor=async(req,res,next)=>
-{
-    try {
-        const id=user.id;
-        const {patient_id,description,medicine,disease}=req.body;
 
-        if(!patient_id)
-        {
-            return res.status(400).send({success:false,message:"Provide patient id"});
-        }
-        const user=await queUserModel.findById(patient_id);
-        await queUserModel.findByIdAndDelete(patient_id);
-        const medicalRecord=await medicalrecordsModel.create({
-            ...user,
-            description,
-            user_id:patient_id,
-            doctor_id:id
 
-        })
+export const TreatedByDoctor = async (req, res, next) => {
+  try {
+    const doctorId = req.user.id;
+    const { patient_id, description, medicine, disease, files } = req.body;
 
-        return res.status(200).json({success:true,message:"Medical REcord added successfully",MedicalREcord:medicalRecord})
-        
-        
-    } catch (error) {
-        next(error);
+    if (!patient_id) {
+      return res.status(400).json({
+        success: false,
+        message: "Provide patient id",
+      });
     }
-}
+
+  
+    const patient = await queUserModel.findById(patient_id).populate("user_id");
+    if (!patient) {
+      return res.status(404).json({
+        success: false,
+        message: "Patient not found in queue",
+      });
+    }
+
+
+    const medicalRecord = await medicalrecordsModel.create({
+      user_id: patient.user_id._id, 
+      doctor_id: doctorId,
+      description,
+      medicine,
+      disease,
+      files: files || [],
+    });
+
+   
+    await queUserModel.findByIdAndDelete(patient_id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Medical record added successfully",
+      medicalRecord,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+ 
--- server/Routes/queueRoutes.js
+++ server/Routes/queueRoutes.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { TreatedByDoctor } from "../Controllers/QueueControllers";
 
 
 
@@ -9,5 +10,6 @@ queueRoutes.get('/get-queue',getOriginalQueue);
 queueRoutes.post('/add-wt-queue',addWtQueue)
 queueRoutes.post('/reject',rejectAppointment)
 queueRoutes.post('/accept',acceptAppointment)
+queueRoutes.post('/add-medical-record',TreatedByDoctor);
 
 export default queueRoutes;
