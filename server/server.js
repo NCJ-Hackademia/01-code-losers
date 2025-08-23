f26834a641a26cdff8b2364af4a5047128feef13
udaykiran1150 <udaykiranponnaganti6@gmail.com>
2025-08-23 16:39:34 +0530

    routes addedd

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 import  express from "express"
 import dotenv from 'dotenv';
 dotenv.config();
-
+import connectDb from "./config/mongodb.js"
+import SendOtpRoute from "./Routes/Sendotp/SendotpRoute.js";
+connectDb();
 const app=express();
-
+app.use(express.json());  
 app.get('/',(req,res)=>
 {
     res.send(`server is working good`);
 })
-
+app.use((err, req, res, next) => {
+  console.error(err.stack); 
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+app.use('/auth',SendOtpRoute)
 app.listen(process.env.PORT,()=>
 {
     console.log(`http://localhost:${process.env.PORT}`);
