7e73903b8c6aa8e10182e4db36b1b60bc61114b5
venkyreddy <venkyreddyy2004@gmail.com>
2025-08-24 09:56:13 +0530

    added my appointments

--- client/src/pages/MyAppointments.jsx
+++ client/src/pages/MyAppointments.jsx
@@ -1,83 +1,86 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {AppContext} from '../context/AppContext'
+import { AppContext } from '../context/AppContext'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const MyAppointments = () => {
-  const {backendUrl,token,getDoctorsData}=useContext(AppContext);
-  const [appointments,setAppointments]=useState([]);
+  const { backendUrl, token, getDoctorsData } = useContext(AppContext);
+  const [appointments, setAppointments] = useState([]);
 
-    const months=['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+ 
+  
 
-    const slotDateFormat=(slotDate)=>{
-      const dateArray=slotDate.split('_')
-      return dateArray[0]+" "+months[Number(dateArray[1])]+" "+dateArray[2]
-    }
+  const getUserAppointments = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/queue/get-queue-details', {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      })
 
-  const getUserAppointments=async()=>{
-    try{
-      const {data}=await axios.get(backendUrl+'/api/user/appointments',{headers:{token}})
-      if(data.success){
+      if (data) {
         setAppointments(data.appointments.reverse())
         console.log(data.appointments)
       }
     }
-    catch(error){
+    catch (error) {
       console.log(error)
       toast.error(error.message)
     }
   }
 
-  const cancelAppointment=async(appointmentId)=>{
-    try{
-      const {data}=await axios.post(backendUrl+'/api/user/cancel-appointment',{appointmentId},{headers:{token}})
-      if(data.success){
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment', { appointmentId }, { headers: { token } })
+      if (data.success) {
         toast.success(data.message)
         getUserAppointments()
-        getDoctorsData
+        getDoctorsData()
       }
-      else{
+      else {
         toast.error(data.message)
       }
     }
-    catch(error){
+    catch (error) {
       console.log(error)
       toast.error(error.message)
     }
   }
 
-  
-
-  useEffect(()=>{
-    if(token){
+  useEffect(() => {
+    if (token) {
       getUserAppointments()
     }
-  },[token])
+  }, [token])
 
   return (
-    <div>
-      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
+    <div className="mt-12">
+      <p className="pb-3 font-medium text-zinc-700 border-b">My Appointments</p>
+
+
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-4 text-sm font-semibold text-neutral-700 py-2 border-b bg-zinc-100 rounded-md mt-2">
+        <p>Doctor Name</p>
+        <p>Specialization</p>
+        <p>Date & Time</p>
+
+      </div>
+
+
       <div>
-        {appointments.map((item,index)=>(
-          <div key={index} className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b'>
-            <div>
-              <img className="w-32 bg-indigo-50" src={item.docData.image}/>
-            </div>
-            <div className='flex-1 text-sm text-zinc-600'>
-              <p className='text-neutral-800 font-semibold'>{item.docData.name}</p>
-              <p>{item.docData.speciality}</p>
-              <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-              <p className='text-xs'>{item.docData.address.line1}</p>
-              <p className='text-xs'>{item.docData.address.line2}</p>
-              <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span>{slotDateFormat(item.slotDate)} |  {item.slotTime}</p>
-            </div>
-            <div></div>
-            <div className='flex flex-col gap-2 justify-end'>
-              {!item.cancelled && item.payment && !item.isCompleted && <button className='sm:min-w-48 py-2 border text-stone-500 bg-indigo-50'>Paid</button>}
-              {!item.cancelled && !item.isCompleted &&<button  onClick={()=>{cancelAppointment(item._id)}} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointent</button>}
-              {item.cancelled && !item.isCompleted && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
-              {item.isCompleted && <button className='sm:min-w-48 py-2 border border-green-500 rounded text-green-500'>Completed</button>}
-            </div>
+        {appointments && appointments.map((item, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-3 sm:grid-cols-4 gap-4 py-3 border-b text-sm text-zinc-600 items-center"
+          >
+            <p className="font-medium text-neutral-800">{item.queue_id.doctor_id.user_id.name}</p>
+            <p>{item.queue_id.doctor_id.specilization}</p>
+            <p>
+              {new Date(item.estimated_time).toLocaleString("en-IN", {
+                dateStyle: "medium",
+                timeStyle: "short",
+              })}
+            </p>
+        
           </div>
         ))}
       </div>
